01726910421d3a12e5ea6a4d8ced67ea08888c2d
junghbb99 <junghbb99@naver.com>
2023-10-23 00:47:40 +0900

    a

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float speed = 10;
+    public float damage = 1;
+    Vector2 direction;
+
+    public Vector2 Direction
+    {
+        set
+        {
+            direction = value.normalized;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,12 +6,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-
     public float speed = 3;
-    
-    
- 
-    
+    Vector3 move;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,25 +17,50 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        move = Vector3.zero;
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
-            transform.Translate(new Vector3(-speed * Time.deltaTime,0,0));
+            move += new Vector3(-1,0,0);
         }
         
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
-            transform.Translate(new Vector3(speed * Time.deltaTime,0,0));
+            move += new Vector3(1,0,0);
         }
         
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
         {
-            transform.Translate(new Vector3(0,speed * Time.deltaTime,0));
+            move += new Vector3(0,1,0);
         }
         
         if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
         {
-            transform.Translate(new Vector3(0,-speed * Time.deltaTime,0));
+            move += new Vector3(0,-1,0);
         }
-        
+
+        if (move.magnitude > 0)
+        {
+            GetComponent<Animator>().SetTrigger("Move");
+        }
+        else
+        {
+            GetComponent<Animator>().SetTrigger("Stop");
+        }
+
+        move = move.normalized;
+        if (move.x < 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+        if (move.x>0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(move * speed * Time.deltaTime);
     }
 }
