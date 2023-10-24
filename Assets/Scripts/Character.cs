3cdc46badd6fb131edf9a9c637ad134a8b3b31ae
junghbb99 <junghbb99@naver.com>
2023-10-25 02:46:40 +0900

    123

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float MaxHP = 3;
+    public GameObject HPGauge;
+    float HP;
+    float HPMaxWidth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HP = MaxHP;
+
+        if (HPGauge != null)
+        { 
+            HPMaxWidth = HPGauge.GetComponent<RectTransform>().sizeDelta.x;
+        }
+
+    }
+
+    public bool Hit(float damage)
+    {
+        HP -= damage;
+        if (HP<0)
+        {
+            HP = 0;
+        }
+
+        if (HPGauge!=null)
+        { 
+            HPGauge.GetComponent<RectTransform>().sizeDelta = new Vector2(HP / MaxHP * HPMaxWidth,
+                HPGauge.GetComponent<RectTransform>().sizeDelta.y);
+        }
+
+
+        return HP > 0;
+    }
+}
